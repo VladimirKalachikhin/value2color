5eaf03fbf00b917df8c3043cc4b8852ddefa75cd
Stager <v.kalachikhin@gmail.com>
2022-11-02 13:35:06 +0300

    first

---
+++ README.md
@@ -0,0 +1,23 @@
+# value2color [![License: CC BY-SA 4.0](https://img.shields.io/badge/License-CC%20BY--SA%204.0-lightgrey.svg)](https://creativecommons.org/licenses/by-sa/4.0/)
+
+A simple JavaScript function to represent a value by a color intermediate between two specified colors.  
+For example, to create a color scale.
+
+## Usage
+```
+<script src="value2color/value2color.js"></script>
+```
+
+```
+color = value2color(value[,minvalue[,maxvalue[,minColor[,maxColor[,underMinColor[,upperMaxColor]]]]]])
+```
+Where:
+* value \<Number> - represented value
+* `minvalue` \<Number> - the begin of the range of values, default 0
+* `maxvalue` \<Number> - the end of the range of values, default 10
+* `minColor` \<Array> or \<String> - the begin of the range of colors, color for `minvalue`, array [r,g,b] or html color string: rgb(r,g,b) or #rrggbb. Default rgb(255,0,0)
+* `maxColor` \<Array> or \<String> - the end of the range of colors, color for `maxvalue`, array [r,g,b] or html color string: rgb(r,g,b) or #rrggbb. Default rgb(0,255,0)
+* `underMinColor` \<String> - color for values less than `minvalue`, html color string: rgb(r,g,b) or #rrggbb. Default rgb(155,0,0)
+* `upperMaxColor` \<String> - color for values more than `maxvalue`, html color string: rgb(r,g,b) or #rrggbb. Default rgb(200,250,240)
+
+Return: \<String> html color string "rgb(r,g,b)"
---
+++ value2color.js
@@ -0,0 +1,28 @@
+function value2color(value,minvalue=0,maxvalue=10,minColor,maxColor,underMinColor="rgb(155,0,0)",upperMaxColor="rgb(200,250,240)"){
+/* Return html color string: rgb(r,g,b).
+If value is between minvalue и maxvalue, then the color returns between minColor and maxColor.
+If value less them minvalue, then returns underMinColor.
+If value more than maxvalue, then returns upperMaxColor.
+minColor,maxColor - array [r,g.b] or html color string: rgb(r,g,b), #rrggbb
+*/
+function colorStrToArray(colorObj){
+	if(Array.isArray(colorObj) && colorObj.length == 3) return colorObj;
+	if(typeof colorObj !== "string") return null;
+	if(colorObj[0] === '#')	return [parseInt(colorObj.substring(1,3),16), parseInt(colorObj.substring(3,5),16), parseInt(colorObj.substring(5),16)];
+	else if (colorObj[0] === 'r') return colorObj.substring(4,colorObj.length-1).split(',').map(str => parseInt(str));
+	else return null;
+}
+
+minColor = colorStrToArray(minColor) || [255,0,0];	// red
+maxColor = colorStrToArray(maxColor) || [0,255,0];	// green
+
+if(value>maxvalue) return upperMaxColor;
+if(value<minvalue) return underMinColor;
+const d = value/Math.abs(maxvalue-minvalue);
+let r = Math.round(minColor[0]+(maxColor[0]-minColor[0])*d);
+let g = Math.round(minColor[1]+(maxColor[1]-minColor[1])*d);
+let b = Math.round(minColor[2]+(maxColor[2]-minColor[2])*d);
+
+return `rgb(${r},${g},${b})`;
+} // end function value2color
+
